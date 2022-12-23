cdbc5b108380f4f88810d4940ada6e8a3137937b
nattanc <naathancaue556@gmail.com>
2022-12-23 18:45:24 -0300

    Initial commit

---
+++ ExercicioPython/ex004.py
@@ -0,0 +1,6 @@
+algo = input('Digite algo: ')
+
+print('É um numero? ', algo.isnumeric())
+print('É alphanumero? ', algo.isalnum())
+print('É decimal ? ', algo.isdecimal())
+print('Esta em maiuscula? ', algo.isupper())
